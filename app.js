336a3174cead5758a0f2a62d7a2e94a8da24ec61
sigdelbjay <sigdelbjay@gmail.com>
2021-05-29 22:58:15 +1000

    Project setup

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+//import routes
+const userRoutes = require('./routes/user')
+
+//app
+const app = express()
+
+//db
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useCreateIndex: true
+}).then(() => console.log('DB connected'))
+
+//routes middleware
+app.use('/api', userRoutes)
+
+const port = process.env.PORT || 8000
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
+
